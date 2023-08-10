e948525808408dde9d788ead6c9277dfb2ac1d6d
prs-sharma9 <prs.sharma9@gmail.com>
2023-08-10 10:50:33 -0600

    Remove html files

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ import session from "express-session";
 import homeRouter from "./routes/home.js";
 import linkedAuthRouter, {initPassport, createLinkedInStrategy} from "./routes/linkedinAuth.js";
 import { connectToDB } from "./DbUtils/dbHandler.js";
+import http from "http";
 
-
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.SERVER_PORT_UNSECURE;
 
 const app = express();
 app.set("view engine", "ejs");
@@ -24,11 +24,26 @@ app.use(initPassport);
 app.use("/", homeRouter);
 app.use("/", linkedAuthRouter);
 
-
-
-app.listen(PORT, () => {
+http.createServer(app).listen(PORT, () => {
   connectToDB()
   createLinkedInStrategy();
   console.log(`Server listening on port ${PORT}`);
 });
 
+// Comment https related code, will be used at later stage
+
+// import https from "https";
+// import fs from "fs";
+
+// const PORT_SECURE = process.env.SERVER_PORT_SECURE
+// const options ={
+//   key: fs.readFileSync(process.env.PRIVATE_KEY),
+//   cert: fs.readFileSync(process.env.PUBLIC_KEY),
+//   passphrase: process.env.PASSPHRASE
+// };
+
+// https.createServer(options, app).listen(PORT_SECURE, () => {
+//   connectToDB()
+//   createLinkedInStrategy();
+//   console.log(`Server listening on port ${PORT_SECURE}`);
+// });
